53bff1e60e0f03cf2042eb6a8e35ba034e0179b4
clonerxyz <clonerxyz@gmail.com>
2023-09-19 16:13:02 +0700

    test3

--- js/app.js
+++ js/app.js
@@ -332,9 +332,20 @@ window.addEventListener('load', () => {
     let name = (new URLSearchParams(window.location.search)).get('to') ?? '';
 
     if (name.length == 0) {
-        document.getElementById('undangan').contentWindow.document.getElementById('namatamu').remove();
-    } else {
+        document.getElementById('namatamu').remove();
+    } elseif {
         let div = document.createElement('div');
+        div.classList.add('m-2');
+        div.innerHTML = `
+        <p class="mt-0 mb-1 mx-0 p-0 text-light">Kepada Yth Bapak/Ibu/Saudara/i</p>
+        <h2 class="text-light">${escapeHtml(name)}</h2>
+        `;
+
+        document.getElementById('formnama').value = name;
+        document.getElementById('namatamu').appendChild(div);
+    }
+		else {
+			let div = document.createElement('div');
         div.classList.add('m-2');
         div.innerHTML = `
         <p class="mt-0 mb-1 mx-0 p-0 text-light">Kepada Yth Bapak/Ibu/Saudara/i</p>
@@ -343,8 +354,9 @@ window.addEventListener('load', () => {
 
         document.getElementById('undangan').contentWindow.document.getElementById('formnama').value = name;
         document.getElementById('undangan').contentWindow.document.getElementById('namatamu').appendChild(div);
-    }
+		}
 
     modal.show();
     opacity();
 }, false);
+
